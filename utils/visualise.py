2c8a3a983cff7775287d10b1db56a824ddbdf930
Alyona Rout <104481226+alyonarout@users.noreply.github.com>
2025-08-29 23:56:28 +0530

    Update visualise.py

--- utils/visualise.py
+++ utils/visualise.py
@@ -1,1 +1,21 @@
+import matplotlib.pyplot as plt
+import torch
+
+def show_reconstruction(input_img, recon_img, target_img=None):
+    fig, axes = plt.subplots(1, 3 if target_img is not None else 2, figsize=(10, 4))
+
+    axes[0].imshow(input_img.squeeze().cpu(), cmap="gray")
+    axes[0].set_title("Input")
+
+    axes[1].imshow(recon_img.squeeze().detach().cpu(), cmap="gray")
+    axes[1].set_title("Reconstruction")
+
+    if target_img is not None:
+        axes[2].imshow(target_img.squeeze().cpu(), cmap="gray")
+        axes[2].set_title("Ground Truth")
+
+    for ax in axes:
+        ax.axis("off")
+    plt.tight_layout()
+    plt.show()
 
