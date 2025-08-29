3047ac3c69b81af9572009e21de86c13385a61d6
Alyona Rout <104481226+alyonarout@users.noreply.github.com>
2025-08-29 23:54:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import yaml
+from trainers.train import Trainer
+
+if __name__ == "__main__":
+    config = yaml.safe_load(open("configs/oasis.yaml"))
+    trainer = Trainer(config)
+    trainer.run()
 
