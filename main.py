3ca276c022d1dfd8a3c6ffa24afa0a0c97e357aa
Alyona Rout <104481226+alyonarout@users.noreply.github.com>
2025-08-30 13:26:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
 import yaml
+
+from trainers.train import Trainer
+from datasets.oasis_dataset import OASISDataset
+from utils.preprocess import preprocess_data
+ 
 from trainers.train import Trainer
 
 if __name__ == "__main__":
