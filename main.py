842829d7c0d01b47376e18897fc9f6416ced01bd
Alyona Rout <104481226+alyonarout@users.noreply.github.com>
2025-08-30 13:21:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
 import yaml
 from trainers.train import Trainer
 
